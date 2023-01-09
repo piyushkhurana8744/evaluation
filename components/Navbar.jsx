48bbf717014124764e083c89155ddcbc4e6225d9
piyushkhurana8744 <piyushkhurana8744@gmail.com>
2023-01-09 14:26:49 +0530

    eval

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -4,7 +4,7 @@ const Navbar = () => {
   return (
     <div style={{display:"flex",justifyContent:"space-around",backgroundColor:"cyan",padding:"10px"}}>
       <Link href="/"><h1>HomePage</h1></Link>
-      <Link href="/Project"><h1>Project</h1></Link>
+      <Link href="https://piyushkhurana8744.github.io/evaluation/Project"><h1>Project</h1></Link>
     </div>
   )
 }
